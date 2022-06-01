ff748fa5afc041153c97784cd0833238ee0a5e68
Michael Wulf <michael.wulf@gmail.com>
2022-06-01 12:41:58 -0500

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'BEADL'
+copyright = '2022, MW'
+author = 'MW'
 
 release = '0.1'
 version = '0.1.0'
